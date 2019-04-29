479c77f478f6c5c952683d12cc90c59ced4872e3
Tine <tine.bertoncelj@gmail.com>
2019-04-29 22:46:42 +0200

    ip changest to TP_link

--- getWeb.py
+++ getWeb.py
@@ -38,7 +38,7 @@ script, file_path, meritev = argv
 
 
 
-contents = urllib.request.urlopen("http://192.168.1.50/data").read()
+contents = urllib.request.urlopen("http://192.168.1.51/data").read()
 
 #contents = "0, 1, 2, 0, 4, 0, 33506"
 str1 = meritevFunc(meritev)
